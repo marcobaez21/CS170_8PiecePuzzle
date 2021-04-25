7649147d7e4b9878c84b45faeaf6baea0f8b213b
marcobaez21 <63010671+marcobaez21@users.noreply.github.com>
2021-04-25 16:26:04 -0700

    Added comments, Generally cleaned up code

--- node.h
+++ node.h
@@ -19,24 +19,24 @@ private:
     Node* right;
     Node* up;
     Problem NodeProblem;
-    Node* moveleft(vector<int>, int, string, int, Node*);
-    Node* movedown(vector<int>, int, string, int, Node*);
-    Node* moveright(vector<int>, int, string, int, Node*);
-    Node* moveup(vector<int>, int, string, int, Node*);
-    int heuristic;
-    int UCS;
-    string heuristicstring;
+    Node* moveleft(vector<int>, int, string, int, Node*); //changes puzzle vector to move the zero left
+    Node* movedown(vector<int>, int, string, int, Node*); // changes puzzle vector to move the zero down
+    Node* moveright(vector<int>, int, string, int, Node*); //changes puzzle vector to move the zero right
+    Node* moveup(vector<int>, int, string, int, Node*); //changes puzzle vector to move the zero up
+    int heuristic; // the "h" value of the heuristic
+    int UCS; // the "g" value of the heuristic
+    string heuristicstring; // used to see if were using eucledian or misplaced
 public:
     Node();
-    Node(vector<int>, string, int, Node*);
-    Node* getParent();
-    Node* getChild(string);
-    vector<int> getPuzzle();
-    void expand();
-    int getHeuristic(string);
-    int g();
-    int h();
-    bool isgoal();
+    Node(vector<int>, string, int, Node*); //constructor with puzzle, string heauristic and parent node*
+    Node* getParent(); // returns parent
+    Node* getChild(string); // returns child
+    vector<int> getPuzzle(); //returns puzzle vector
+    void expand(); //expands a node*, uses the moveleft-moveup functions
+    int getHeuristic(string); //calculates and returns heaurstic value
+    int g(); //returns UCS or "g" value
+    int h(); //returns heauristic or "h" value
+    bool isgoal(); //returns bool of current node is goal puzzle
 
 };
 
--- tree.h
+++ tree.h
@@ -12,7 +12,7 @@
 #include <queue>
 using namespace std;
 
-struct smallerheuristic{
+struct smallerheuristic{ //Honestly still not sure how or why this works, but i got this code from the link above
     bool operator()(Node* one, Node* two){
         return ((one->g()+one->h())>(two->g()+two->h()));
     }
@@ -20,20 +20,20 @@ struct smallerheuristic{
 
 class Tree {
 private:
-    int nodescurrinqueue;
-    int maxnodesinqueue;
-    priority_queue<Node*, vector<Node*>, smallerheuristic> frontier; //FIX
-    vector<Node*>exploredlist;
-    Node* root;
+    int nodescurrinqueue; //variable that counts how many nodes are currently in queue
+    int maxnodesinqueue; //varaible that counts the max number of nodes in queue at once
+    priority_queue<Node*, vector<Node*>, smallerheuristic> frontier; // priority queue used for searching through tree
+    vector<Node*>exploredlist; //vector used for searched nodes
+    Node* root; //root of tree
 public:
     Tree();
-    Tree(Node*);
-    bool hasbeenexplored(Node*);
-    void addtoexplored(Node*);
-    priority_queue<Node*, vector<Node*>, smallerheuristic> getfrontier();
-    void addtofrontier(Node*);
-    Node* popfromfrontier();
-    int getmaxnodesinqueue();
+    Tree(Node*); //constructor with root
+    bool hasbeenexplored(Node*); //checks if passed in Node is in explored list, returns bool
+    void addtoexplored(Node*); //adds passed in Node* to explored list
+    priority_queue<Node*, vector<Node*>, smallerheuristic> getfrontier(); //returns priority queue
+    void addtofrontier(Node*); // adds passed in Node* to frontier
+    Node* popfromfrontier(); //pops top node from frontier, returns popped node*
+    int getmaxnodesinqueue(); //returns max nodes in queue
     void testtree();
 
 
